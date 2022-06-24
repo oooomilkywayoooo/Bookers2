b774a84e6f1085adf115145e2c40d2e4dc1cd536
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-06-24 15:09:56 +0000

    [Add]User Show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,33 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.new
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(@book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+end
+
+def user_params_update
+  params.require(:user).permit(:name, :email, :image, :introduction)
 end
