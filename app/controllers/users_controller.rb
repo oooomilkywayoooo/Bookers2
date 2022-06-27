f853eb2833bf69fa0385a52d844c82528d090f3d
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-06-27 13:14:38 +0000

    [Fix]useredit,bookedit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,20 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+      if @user == current_user
+        render "edit"
+      else
+        redirect_to books_path
+      end
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
-      redirect_to user_path(user.id)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
     end
   end
 
